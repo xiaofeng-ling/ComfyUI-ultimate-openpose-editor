06be80f4b335528e21e7e125f8c3ff4bae315140
RyanOnTheInside <7623207+ryanontheinside@users.noreply.github.com>
2025-06-04 16:41:24 -0400

    appendage editor

--- __init__.py
+++ __init__.py
@@ -1,14 +1,19 @@
 from .openpose_editor_nodes import OpenposeEditorNode
+from .appendage_editor_nodes import AppendageEditorNode, PoseRendererNode
 
 
 WEB_DIRECTORY = "js"
 
 NODE_CLASS_MAPPINGS = {
     "OpenposeEditorNode": OpenposeEditorNode,
+    "AppendageEditorNode": AppendageEditorNode,
+    "PoseRendererNode": PoseRendererNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "OpenposeEditorNode": "Openpose Editor Node",
+    "AppendageEditorNode": "Appendage Editor",
+    "PoseRendererNode": "Pose Renderer",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]

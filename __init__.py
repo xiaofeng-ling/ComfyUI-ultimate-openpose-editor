a83728d68d641f68d85a19701e7fc6864057ae53
mhstar <143965219+westNeighbor@users.noreply.github.com>
2025-06-04 17:07:44 -0500

    Update __init__.py
    remove pose render node cause already having a separate one

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 from .openpose_editor_nodes import OpenposeEditorNode
-from .appendage_editor_nodes import AppendageEditorNode, PoseRendererNode
+from .appendage_editor_nodes import AppendageEditorNode
 
 
 WEB_DIRECTORY = "js"
@@ -7,13 +7,11 @@ WEB_DIRECTORY = "js"
 NODE_CLASS_MAPPINGS = {
     "OpenposeEditorNode": OpenposeEditorNode,
     "AppendageEditorNode": AppendageEditorNode,
-    "PoseRendererNode": PoseRendererNode,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "OpenposeEditorNode": "Openpose Editor Node",
     "AppendageEditorNode": "Appendage Editor",
-    "PoseRendererNode": "Pose Renderer",
 }
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
